4e354e1c1eb807046a74447a533314df7b3d27ad
saurav3232 <sauravkumar48736@gmail.com>
2022-07-14 22:25:05 +0530

    Completed App profile

--- app.js
+++ app.js
@@ -113,7 +113,10 @@ app.get("/:customListname", (req, res) => {
 app.get("/about", function (req, res) {
   res.render("about");
 });
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log(`Server started on port ${port}`);
 });
